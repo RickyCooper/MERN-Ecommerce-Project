dcf3eee1dc7a1900a1ce368d2944779d0db3b2e6
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-12 12:19:54 +0100

    Set up React-Bootstrap
    Created header and footer components and styled them using react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './app.module.scss';
 
+import {Container} from 'react-bootstrap'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to pro shop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to pro shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
+import './bootstrap.min.css'
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
