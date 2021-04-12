fe8a69820e1e68c60548d7684a8044e3bc455b0b
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-12 13:22:25 +0100

    Rating Component
    Created a rating component using conditional ternary operators switching between different font awesome icons.

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,4 +1,5 @@
 import {Card} from 'react-bootstrap';
+import Rating from '../Rating/Rating';
 import React from 'react';
 
 const Product = ({product}) => {
@@ -14,7 +15,7 @@ const Product = ({product}) => {
                 </a>
                 <Card.Text as='div'>
                     <div className="my-3">
-                        {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
                     </div>
                 </Card.Text>
                 <Card.Text as='h3'>${product.price}
