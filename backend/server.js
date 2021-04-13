d7399c5397f926cee4e20bf2b05bc2620420edf3
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-13 11:01:31 +0100

    Connected the MongoDB database.
    Also created three Mongoose schemas configuring, types and default values for orderModel,  userModel and productModel.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+import colors from 'colors';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import express from 'express';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB(); 
 
 const app = express();
 
@@ -23,7 +27,7 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`.yellow.bold))
 
 /* inside your frontend package.json file paste this under "name" ->  "proxy": "http://127.0.0.1:5000"
 this is a proxy that instead of looking at local host 3000 your frontend will send reqests to localhost 5000*/ 
