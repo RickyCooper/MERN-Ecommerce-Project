a5b00a1442bc1fff65a5a4a8581868bb2ebfca66
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-13 12:33:28 +0100

    API 404 error handler
    Created an error handling middleware, that will set the status code, and return a json object with the error message.

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
 import colors from 'colors';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
@@ -16,9 +18,13 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`.yellow.bold))
 
 /* inside your frontend package.json file paste this under "name" ->  "proxy": "http://127.0.0.1:5000"
 this is a proxy that instead of looking at local host 3000 your frontend will send reqests to localhost 5000*/ 
+
