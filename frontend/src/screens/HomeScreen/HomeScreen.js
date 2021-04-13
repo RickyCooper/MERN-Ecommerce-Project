78149496c514b476df39f4d02311652e6202e914
Ricky Cooper <61281472+kr0mby@users.noreply.github.com>
2021-04-13 14:22:06 +0100

    Redux reducer set up
    Created actions to dispatch fetch request to the server asynchronously using react-thunk now the HomeScreen will dispatch the actions in useEffect and we can handle that state data assign them to variables and render them to the dom.

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -1,32 +1,31 @@
 import {Col, Row} from 'react-bootstrap';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux'
 
 import Product from '../../components/Product/Product';
-import axios from 'axios';
+import {listProducts} from '../../actions/productActions'
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products')
+    const productList = useSelector(state => state.productList)
+    const {loading, error, products} = productList
 
-            setProducts(data)
-        }
-        fetchProducts();
-    },[]);
+    useEffect(() => {   
+        dispatch(listProducts())
+    },[dispatch]);
     
-    const [products, setProducts] = useState([]);
-
     return (
         <>
             <h1>Lastest Products</h1>
-            <Row>
+            {loading ? <h2>Loading</h2> : error ? <h3>{error}</h3> :            
+             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                         <Product product={product}/>
                     </Col>
                 ))}
-            </Row>
+            </Row>}
         </>
     )
 }
---
+++ frontend/src/store/store.js
@@ -0,0 +1,17 @@
+import { appleMiddleware, applyMiddleware, combineReducers, createStore } from 'redux';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {productListReducer} from '../reducers/productListReducers';
+import thunk from 'redux-thunk';
+
+const reducer = combineReducers({
+    productList: productListReducer
+});
+
+const initialState = {};
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store;
